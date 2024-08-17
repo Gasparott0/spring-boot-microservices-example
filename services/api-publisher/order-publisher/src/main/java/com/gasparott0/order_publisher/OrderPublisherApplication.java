9cd0c1a475ef66a255675487f2ea8630a72ee416
Gasparott0 <mtsgasparotto@hotmail.com>
2024-08-17 17:54:01 -0300

    first commit

---
+++ services/api-publisher/order-publisher/src/main/java/com/gasparott0/order_publisher/OrderPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.gasparott0.order_publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderPublisherApplication.class, args);
+	}
+
+}
